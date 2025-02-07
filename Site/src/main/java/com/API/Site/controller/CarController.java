748dcacdf3c407bf4d554688fb543ce59aee62e4
davassw <davadsssw@gmail.com>
2025-02-07 21:28:27 +0400

    DavudCarSale(changed, добавлен фронтенд)
    Добавил фронтенд.

--- Site/src/main/java/com/API/Site/controller/CarController.java
+++ Site/src/main/java/com/API/Site/controller/CarController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.API.Site.entity.CarEntity;
 import com.API.Site.service.CarService;
-
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RestController
 @RequestMapping("/cars")
 public class CarController {
